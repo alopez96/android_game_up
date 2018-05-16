20184541d22f6d7f95ca94cf73485fac2746fae3
Arturo Lopez <arturolopez@dhcp-169-233-142-244.cruznet.ucsc.edu>
2018-05-16 10:17:04 -0700

    login works for app

--- app/src/main/java/ca/alexbalt/gameup2/MainActivity.java
+++ app/src/main/java/ca/alexbalt/gameup2/MainActivity.java
@@ -1,6 +1,7 @@
 package ca.alexbalt.gameup2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,14 +10,52 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "main_activity";
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseAuth mAuth;
+    public static final int RC_SIGN_IN = 123;     ///request code
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //user is signed in
+                    Toast.makeText(MainActivity.this,"Now signed in!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //user is signed out
+                    startActivityForResult(
+                            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
+
     }
     @Override
    public boolean onCreateOptionsMenu(Menu menu){
@@ -69,6 +108,24 @@ public class MainActivity extends AppCompatActivity {
    }
 
 
+
+
+
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        //ad listener
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        //remove listener
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
 
    
 }
